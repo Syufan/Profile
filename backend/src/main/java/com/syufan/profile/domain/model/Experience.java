076159e16b2e8e8b5987bef451ba87c68baaa8ba
Syufan <zyf571635687@gmail.com>
2026-02-27 02:02:36 +0800

    refactor: simplify Experience url to String

--- backend/src/main/java/com/syufan/profile/domain/model/Experience.java
+++ backend/src/main/java/com/syufan/profile/domain/model/Experience.java
@@ -1,7 +1,6 @@
 package com.syufan.profile.domain.model;
 
 import java.util.List;
-import java.util.Map;
 import lombok.Data;
 
 @Data
@@ -9,7 +8,7 @@ public class Experience {
     private String period;
     private String company;
     private String role;
-    private Map<String, String> url;
+    private String url;
     private List<String> bullets;
     private List<String> techStack;
 }
