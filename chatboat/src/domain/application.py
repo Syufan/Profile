2119e20aaf8f4a32e1f192f208a5f931edb0e03c
Syufan <zyf571635687@gmail.com>
2026-03-05 01:12:10 +0800

    feat: load suggestions from yaml

---
+++ chatboat/src/domain/application.py
@@ -0,0 +1,15 @@
+import yaml
+
+SUGGESTIONS_LIMIT = 3
+
+class Application:
+    def __init__(self, db_path: str) -> None:
+        self._suggestions = self._load_suggestions(db_path)
+
+
+    def _load_suggestions(self, db_path:str) -> list[str]:
+        with open(db_path) as f:
+            data = yaml.safe_load(f)
+        if len(data["suggestions"])<3:
+            raise ValueError("Not enough suggestions")
+        return data["suggestions"]
