95f435a685b4cdec7781db4302263ad1ca04fdd4
Syufan <zyf571635687@gmail.com>
2026-03-11 13:59:02 +0800

    test: add send message router

--- chatboat/src/web/test/test_chatboat.py
+++ chatboat/src/web/test/test_chatboat.py
@@ -1,10 +1,21 @@
-from src.web.routes.chatboat import ChatBoatRoutes
-from src.domain.application import Application
+from src.web.routes.chatboat import ChatBoatRoutes, ChatRequest
+from unittest.mock import MagicMock
 
-def test_successfully_get_3_suggestions(tmp_path):
-    fake_path = tmp_path / "suggestions.yaml"
-    fake_path.write_text("suggestions:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
-    app = Application(fake_path)
-    routes = ChatBoatRoutes(app)
+def test_successfully_get_3_suggestions():
+    mock_app = MagicMock()
+    mock_app.pick_random_suggestion.return_value = ["hello", "hey", "okay"]
+    routes = ChatBoatRoutes(mock_app)
 
     assert len(routes.get_random_suggestions()["suggestions"]) == 3
+    mock_app.pick_random_suggestion.assert_called_once()
+
+def test_send_message():
+    mock_app = MagicMock()
+    mock_app.send_message.return_value = "Hello"
+    routes = ChatBoatRoutes(mock_app)
+
+    request = ChatRequest(message="hello")
+    result = routes.send_message(request)
+
+    mock_app.send_message.assert_called_once()
+    assert result.body != b""
