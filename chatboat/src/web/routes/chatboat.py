b9d6f7c13602b023362c791d382d04e0d0d8ebc1
Syufan <zyf571635687@gmail.com>
2026-03-11 14:00:12 +0800

    feat: add send message function

--- chatboat/src/web/routes/chatboat.py
+++ chatboat/src/web/routes/chatboat.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 
 from src.domain.application import Application
 
+class ChatRequest(BaseModel):
+    message: str
 
 class ChatBoatRoutes:
     def __init__(self, application: Application) -> None:
@@ -15,3 +19,6 @@ class ChatBoatRoutes:
 
     def get_random_suggestions(self) -> dict:
         return {"suggestions": self._application.pick_random_suggestion()}
+
+    def send_message(self, request: ChatRequest) -> JSONResponse:
+        return JSONResponse(content={"message": self._application.send_message(request.message)})
